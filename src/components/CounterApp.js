aa38f64a15df5f1fee43fe5a5ebf27f7998894bc
halil <halilskywater@gmail.com>
2022-04-07 10:51:51 -0600

    fire

--- src/components/CounterApp.js
+++ src/components/CounterApp.js
@@ -7,18 +7,15 @@ import Hide from "./Hide";
 function CounterApp() {
   const [counter, setCounter] = useState(0);
   const [title, setTitle] = useState(false);
-  const [toggle,setToggle]= useState(false)
+  const [toggle, setToggle] = useState(false);
 
   const changetext = () => {
-    setTitle((prev)=>!prev);
+    setTitle((prev) => !prev);
   };
 
-  const HideCounter=()=>{
-    setToggle((prev)=> !prev);
-   
-  }
-
- 
+  const HideCounter = () => {
+    setToggle((prev) => !prev);
+  };
 
   const increase = () => {
     setCounter(counter + 1);
@@ -29,15 +26,14 @@ function CounterApp() {
   };
   const resset = () => {
     setCounter(0);
-    
   };
 
   return (
     <container className="body">
       <div className="counter-container">
-        <h1 className={title ?"count-title": 'change-title'}>Counter</h1>
+        <h1 className={title ? "count-title" : "change-title"}>Counter</h1>
 
-        <h1 className={toggle ? 'count-number' : 'hide'}>{counter}</h1>
+        <h1 className={toggle ? "count-number" : "hide"}>{counter}</h1>
         <div className="buttons">
           <Hide HideCounter={HideCounter} setCounter={setCounter}></Hide>
           <Count
@@ -46,7 +42,7 @@ function CounterApp() {
             reduce={reduce}
             resset={resset}
           ></Count>
-          <Change title={title}  changetext={changetext}></Change>
+          <Change title={title} changetext={changetext}></Change>
         </div>
       </div>
     </container>
